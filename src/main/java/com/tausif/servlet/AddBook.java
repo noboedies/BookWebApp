8dd24478fae8edebaf95be1b67b3ff9bb3734361
Tausif alam <121092701+noboedies@users.noreply.github.com>
2026-03-19 13:21:30 +0530

    Enhance AddBook servlet with user session management
    Added user association to the book and improved session handling.

--- src/main/java/com/tausif/servlet/AddBook.java
+++ src/main/java/com/tausif/servlet/AddBook.java
@@ -8,6 +8,7 @@ import org.hibernate.Transaction;
 
 import com.tausif.HbUtility;
 import com.tausif.entity.Book;
+import com.tausif.entity.User;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
@@ -15,6 +16,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.Part;
 
 /**
@@ -36,64 +38,58 @@ public class AddBook extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Part p1 = request.getPart("cImage");
-		Part p2 = request.getPart("ctn");
 		
-		byte[] image = p1.getInputStream().readAllBytes();
-		byte[] content = p2.getInputStream().readAllBytes();
-		
-		if(image.length > 2*1000*1000 && content.length > 10*1000*1000) {
-			response.sendRedirect("fileSizeExceed.html");
-		} else if(image.length > 2*1000*1000) {
-			response.sendRedirect("imageSizeExceed.html");
-		} else if(content.length > 10*1000*1000) {
-			response.sendRedirect("contentSizeExceed.html");
+		HttpSession httpSession = request.getSession(false);
+		if(httpSession == null) {
+			response.sendRedirect("session_error.html");
 		} else {
-			String name = request.getParameter("name");
-			String aname = request.getParameter("aname");
-			String pname = request.getParameter("pname");
-			int price = Integer.parseInt(request.getParameter("price"));
+			User user = (User)httpSession.getAttribute("user");
+			Part p1 = request.getPart("cImage");
+			Part p2 = request.getPart("ctn");
 			
-			Session session = HbUtility.sessionFactory.openSession();
-			Transaction transaction = session.beginTransaction();
+			byte[] image = p1.getInputStream().readAllBytes();
+			byte[] content = p2.getInputStream().readAllBytes();
 			
-			Book book = session.get(Book.class, name);
-			
-			if(book == null) {
-				Book b = new Book();
-				b.setName(name);
-				b.setAname(aname);
-				b.setPname(pname);
-				b.setPrice(price);
-				session.persist(b);
-				transaction.commit();
-				session.close();
-				
-				PrintWriter out = response.getWriter();
-				out.print("<!DOCTYPE html>");
-				out.print("<html>");
-				out.print("<body>");
-				out.print("<h1> Book App </h1>");
-				out.print("<hr>");
-				out.print("<p style = 'color:green;'> Book Added Successfully </p>");
-				out.print("<a href='index.html'>Return to Home</a>");
-				out.print("</body");
-				out.print("</html>");
-				out.close();
+			if(image.length > 2*1000*1000 && content.length > 10*1000*1000) {
+				response.sendRedirect("fileSizeExceed.html");
+			} else if(image.length > 2*1000*1000) {
+				response.sendRedirect("imageSizeExceed.html");
+			} else if(content.length > 10*1000*1000) {
+				response.sendRedirect("contentSizeExceed.html");
 			} else {
-				PrintWriter out = response.getWriter();
-				out.print("<!DOCTYPE html>");
-				out.print("<html>");
-				out.print("<body>");
-				out.print("<h1> Book App </h1>");
-				out.print("<hr>");
-				out.print("<p style = 'color:red;'> Book Already Exist</p>");
-				out.print("<a href='index.html'>Return to Home</a>");
-				out.print("</body>");
-				out.print("</html>");
-				out.close();
+				String name = request.getParameter("name");
+				String aname = request.getParameter("aname");
+				String pname = request.getParameter("pname");
+				int price = Integer.parseInt(request.getParameter("price"));
+				
+				Session session = HbUtility.sessionFactory.openSession();
+				Transaction transaction = session.beginTransaction();
+				
+				Book book = session.get(Book.class, name);
+				
+				if(book == null) {
+					Book b = new Book();
+					b.setName(name);
+					b.setAname(aname);
+					b.setPname(pname);
+					b.setPrice(price);
+					b.setUser(user);
+					
+					if(content.length > 0) {
+						b.setContent(content);
+					}
+					
+					session.persist(b);
+					transaction.commit();
+					session.close();
+					response.sendRedirect("success.html");
+				} else {
+					response.sendRedirect("exist.html");
+				}
 			}
 		}
+		
+		
 	}
 
 }
