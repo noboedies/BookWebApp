97a919a96f19d919b10b7eba6479d095c8a93a2a
Tausif alam <121092701+noboedies@users.noreply.github.com>
2026-03-24 20:10:53 +0530

    Refactor UserHome servlet for improved functionality

--- src/main/java/com/tausif/servlet/UserHome.java
+++ src/main/java/com/tausif/servlet/UserHome.java
@@ -2,7 +2,12 @@ package com.tausif.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
+import org.hibernate.Session;
+
+import com.tausif.HbUtility;
+import com.tausif.entity.Book;
 import com.tausif.entity.User;
 
 import jakarta.servlet.ServletException;
@@ -13,7 +18,7 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.annotation.WebServlet;
 
 /**
- * Servlet implementation class UserHome
+ * Servlet implementation class SearchBook
  */
 @WebServlet("/UserHome")
 public class UserHome extends HttpServlet {
@@ -30,37 +35,42 @@ public class UserHome extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession httpSession = request.getSession(false);
-		if(httpSession == null) {
+		HttpSession session=request.getSession(false);
+		if(session==null) {
 			response.sendRedirect("session_error.html");
-			
-		} else {
-			User user = (User)httpSession.getAttribute("User");
-			PrintWriter out = response.getWriter();
+		}else {
+			User user=(User)session.getAttribute("user");
+			PrintWriter out=response.getWriter();
 			out.print("<!DOCTYPE html>");
 			out.print("<html>");
 			out.print("<body>");
-			out.print("<h1>BookApp</h1>");
+			out.print("<h1>Incapp Book App</h1>");
 			out.print("<hr>");
 			out.print("Welcome: <b> "+user.getName()+" </b>");
-			out.print("<a href='Logout'>Logout</a>");
+			out.print("&nbsp;&nbsp;<a href='UserHome'>Home</a>");
+			out.print("&nbsp;&nbsp;<a href='AllBooks'>All Books</a>");
+			out.print("&nbsp;&nbsp;<a href='Logout'>Logout</a>");
 			out.print("<hr>");
+			String msg=(String)session.getAttribute("msg");
+			if(msg!=null) {
+				out.print("<p>"+msg+"</p><hr>");
+				session.setAttribute("msg",null);
+			}
 			out.print("<form action=\"AddBook\" method=\"post\" enctype=\"multipart/form-data\">\r\n"
-					+ "       <label>Name:</label>\r\n"
-					+ "       <input type='text' name='name' required /> <br><br>\r\n"
-					+ "       <label>Author Name:</label>\r\n"
-					+ "       <input type='text' name='aname' required /> <br><br>\r\n"
-					+ "       <label>Publisher Name:</label>\r\n"
-					+ "       <input type='text' name='pname' required /> <br><br>\r\n"
-					+ "       <label>Price:</label>\r\n"
-					+ "       <input type='number' min='1' name='price' required /> <br><br>\r\n"
-					+ "       <label>Cover Image:</label>\r\n"
-					+ "       <input type='file' name='cImage' accept=\"image/*\" /> <br><br>\r\n"
-					+ "       <label>Book Pdf:</label>\r\n"
-					+ "       <input type='file' name='ctn' accept=\".pdf\" /> <br><br>\r\n"
-					+ "       <button>Add</button>\r\n"
-					+ "   </form>");
+					+ "		<label>Name:</label>\r\n"
+					+ "		<input type='text' name='name' required /> <br><br>\r\n"
+					+ "		<label>Author Name:</label>\r\n"
+					+ "		<input type='text' name='aname' required /> <br><br>\r\n"
+					+ "		<label>Publisher Name:</label>\r\n"
+					+ "		<input type='text' name='pname' required /> <br><br>\r\n"
+					+ "		<label>Price:</label>\r\n"
+					+ "		<input type='number' min='1' name='price' required /> <br><br>\r\n"
+					+ "		<label>Cover Image:</label>\r\n"
+					+ "		<input type='file' name='cImage' accept=\"image/*\" /> <br><br>\r\n"
+					+ "		<label>Book Pdf:</label>\r\n"
+					+ "		<input type='file' name='ctn' accept=\".pdf\" /> <br><br>\r\n"
+					+ "		<button>Add</button>\r\n"
+					+ "	</form>");
 			out.print("</body>");
 			out.print("</html>");
 			out.close();
