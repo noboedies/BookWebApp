bcce8a38b01eed87b7489cedd7a8fd89f93f6c33
Tausif alam <121092701+noboedies@users.noreply.github.com>
2026-03-24 20:08:33 +0530

    Refactor AllBooks servlet for improved functionality

--- src/main/java/com/tausif/servlet/AllBooks.java
+++ src/main/java/com/tausif/servlet/AllBooks.java
@@ -11,14 +11,14 @@ import com.tausif.entity.Book;
 import com.tausif.entity.User;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
- * Servlet implementation class AllBooks
+ * Servlet implementation class SearchBook
  */
 @WebServlet("/AllBooks")
 public class AllBooks extends HttpServlet {
@@ -35,34 +35,37 @@ public class AllBooks extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession httpSession = request.getSession(false);
-		if(httpSession == null) {
+		HttpSession session=request.getSession(false);
+		if(session==null) {
 			response.sendRedirect("session_error.html");
 		}else {
-			User user = (User)httpSession.getAttribute("User");
-			PrintWriter out = response.getWriter();
+			User user=(User)session.getAttribute("user");
+			PrintWriter out=response.getWriter();
 			out.print("<!DOCTYPE html>");
 			out.print("<html>");
 			out.print("<body>");
-			out.print("<h1>Tausif Book App</h1>");
+			out.print("<h1>Incapp Book App</h1>");
 			out.print("<hr>");
 			out.print("Welcome: <b> "+user.getName()+" </b>");
 			out.print("&nbsp;&nbsp;<a href='UserHome'>Home</a>");
-			out.print("&nbsp;&nbsp;<a href='Logout'>logout</a>");
+			out.print("&nbsp;&nbsp;<a href='AllBooks'>All Books</a>");
+			out.print("&nbsp;&nbsp;<a href='Logout'>Logout</a>");
 			out.print("<hr>");
-			String msg = (String)httpSession.getAttribute("msg");
-			if(msg != null) {
-				out.print("<p> "+msg+" </p><hr>");
-				httpSession.setAttribute("msg", null);
+			String msg=(String)session.getAttribute("msg");
+			if(msg!=null) {
+				out.print("<p>"+msg+"</p><hr>");
+				session.setAttribute("msg",null);
 			}
 			
-			Session session = HbUtility.sessionFactory.openSession();
-			List<Book> books = session.createQuery("select b from Book b where b.user = :user",Book.class)
-			.setParameter("User", user).list();
+			Session ses=HbUtility.sessionFactory.openSession();
+			//All Books
+//			List<Book> books=ses.createQuery("from Book",Book.class).list();
 			
+			//All Books a/c to user
+			List<Book> books=ses.createQuery("select b from Book b where b.user = :user",Book.class)
+					.setParameter("user", user ).list();
 			if(books.isEmpty()) {
-				out.print("<p>No Book Found</p>");
+				out.print("<p>No Book Found!</p>");
 			}else {
 				out.print("<section style='display:flex;flex-wrap:wrap;'>");
 				for(Book b:books) {
@@ -70,12 +73,11 @@ public class AllBooks extends HttpServlet {
 					if(b.getImage()!=null) {
 						out.print("<img src='GetImage?name="+b.getName()+"' height='100px' />");
 					}else {
-						out.print("<img src='book.png' height='100px' />");
+						out.print("<img src='book.jpg' height='100px' />");
 					}
-					
 					out.print("<p>Name: "+b.getName()+" </p>");
-					out.print("<p>Price: "+b.getPrice()+"</p>");
-					out.print("<hr>");
+					out.print("<p>Price: "+b.getPrice()+" </p>");
+					out.print("<hr/>");
 					out.print("<form action='BookDetails' method='post'>");
 					out.print("<input type='hidden' name='name' value='"+b.getName()+"'>");
 					out.print("<button>More Details</button>");
@@ -88,7 +90,6 @@ public class AllBooks extends HttpServlet {
 			out.print("</body>");
 			out.print("</html>");
 			out.close();
-			
 		}
 	}
 
