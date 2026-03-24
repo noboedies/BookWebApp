6168732e4ace6d1015e143d878cc42e1ab62864d
Tausif alam <121092701+noboedies@users.noreply.github.com>
2026-03-24 20:08:09 +0530

    Refactor AddBook servlet for image and content handling
    Updated image handling and response redirects.

--- src/main/java/com/tausif/servlet/AddBook.java
+++ src/main/java/com/tausif/servlet/AddBook.java
@@ -11,13 +11,13 @@ import com.tausif.entity.Book;
 import com.tausif.entity.User;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.Part;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
  * Servlet implementation class AddBook
@@ -38,58 +38,52 @@ public class AddBook extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession httpSession = request.getSession(false);
-		if(httpSession == null) {
+		HttpSession session=request.getSession(false);
+		if(session==null) {
 			response.sendRedirect("session_error.html");
-		} else {
-			User user = (User)httpSession.getAttribute("user");
-			Part p1 = request.getPart("cImage");
-			Part p2 = request.getPart("ctn");
-			
-			byte[] image = p1.getInputStream().readAllBytes();
-			byte[] content = p2.getInputStream().readAllBytes();
-			
+		}else {
+			User user=(User)session.getAttribute("user");
+			Part p1=request.getPart("cImage");
+			Part p2=request.getPart("ctn");
+			byte[] image=p1.getInputStream().readAllBytes();
+			byte[] content=p2.getInputStream().readAllBytes();
 			if(image.length > 2*1000*1000 && content.length > 10*1000*1000) {
-				response.sendRedirect("fileSizeExceed.html");
-			} else if(image.length > 2*1000*1000) {
+				response.sendRedirect("filesSizeExceed.html");
+			}else if(image.length > 2*1000*1000) {
 				response.sendRedirect("imageSizeExceed.html");
-			} else if(content.length > 10*1000*1000) {
+			}else if(content.length > 10*1000*1000) {
 				response.sendRedirect("contentSizeExceed.html");
-			} else {
+			}else {
 				String name = request.getParameter("name");
 				String aname = request.getParameter("aname");
 				String pname = request.getParameter("pname");
 				int price = Integer.parseInt(request.getParameter("price"));
-				
-				Session session = HbUtility.sessionFactory.openSession();
-				Transaction transaction = session.beginTransaction();
-				
-				Book book = session.get(Book.class, name);
-				
-				if(book == null) {
+				Session ses=HbUtility.sessionFactory.openSession();
+				Transaction transaction=ses.beginTransaction();
+				Book book=ses.get(Book.class, name);
+				if(book==null) {
 					Book b = new Book();
 					b.setName(name);
 					b.setAname(aname);
 					b.setPname(pname);
 					b.setPrice(price);
 					b.setUser(user);
-					
+					if(image.length > 0) {
+						b.setImage(image);
+					}
 					if(content.length > 0) {
 						b.setContent(content);
 					}
-					
-					session.persist(b);
+					ses.persist(b);
 					transaction.commit();
-					session.close();
-					httpSession.setAttribute("msg", "Book Added Successfully!");
-				} else {
-					httpSession.setAttribute("msg", "Book Name Already Exists!");
+					ses.close();
+					session.setAttribute("msg","Book Added Successfully!");
+				}else {
+					session.setAttribute("msg","Book Name Already Exist!");
 				}
+				response.sendRedirect("UserHome");
 			}
 		}
-		
-		
 	}
 
 }
